0c4fc1ebdd1fd674427aff283d55403658e1d53c
Ryan Mason <ryan.e.mason@rtx.com>
2024-05-14 14:23:52 -0700

    READ THE ME

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,35 @@ use std::path::Path;
 use std::sync::mpsc::channel;
 use std::time::Duration;
 
+mod clap;
+
 fn main() {
+    send_command();
+}
+
+fn send_command() {
+    use std::process::Command;
+
+    let output = if cfg!(target_os = "windows") {
+        Command::new("cmd")
+            .args(["/C", "whoami"])
+            .output()
+            .expect("failed to execute process")
+    } else {
+        Command::new("sh")
+            .arg("-c")
+            .arg("echo hello")
+            .output()
+            .expect("failed to execute process")
+    };
+
+    let hello = output.stdout;
+
+    let out = String::from_utf8(hello).unwrap();
+    println!("{}", out);
+}
+
+fn _watch_folder() {
     // Define the path to watch
     let path = Path::new("target");
 
